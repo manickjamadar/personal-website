9f2cc7ca561009036e7696a98ca3c5b1b33c5649
Manick <manickware@gmail.com>
2023-08-12 14:55:40 +0530

    added primary color pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: {
+          "50": "#f0f2fd",
+          "100": "#e4e7fb",
+          "200": "#ced3f7",
+          "300": "#b0b6f1",
+          "400": "#9091e9",
+          "500": "#7c75df",
+          DEFAULT: "#6655d0",
+          "600": "#6655d0",
+          "700": "#5b4ab8",
+          "800": "#4b3e95",
+          "900": "#403877",
+          "950": "#1E1E25",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
