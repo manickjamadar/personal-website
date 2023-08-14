9c3444d3ef64a5e8165745a724a753f1502f600c
Manick <manickware@gmail.com>
2023-08-14 17:01:51 +0530

    initial skill section created

---
+++ app/components/sections/skillSection.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import SectionHeader from "../sectionHeader";
+import { FaNodeJs } from "react-icons/fa";
+import { SiFirebase } from "react-icons/si";
+import SkillList from "../skillList";
+import CategoryFilter from "../categoryFilter";
+import mySkills from "@/app/data/skills";
+const SkillSection = () => {
+  return (
+    <div className="flex flex-col gap-6 items-center">
+      <SectionHeader
+        title="My Creative Skills"
+        subtitle="Check out my awesome creative skills"
+      />
+      <CategoryFilter categories={["All", "Frontend", "Backend", "Other"]} />
+      <SkillList skills={mySkills} />
+    </div>
+  );
+};
+
+export default SkillSection;
