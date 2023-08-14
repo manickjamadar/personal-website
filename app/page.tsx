cd9dec08c40d3dda870b12b4e1abcb8002b1d3bf
Manick <manickware@gmail.com>
2023-08-14 13:17:26 +0530

    hero section added in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Container from "./components/Container";
 import Header from "./components/sections/header";
+import Hero from "./components/sections/hero";
+import Spacer from "./components/spacer";
 
 const HomePage = () => {
   return (
     <Container>
       <Header />
+      <Spacer vertical={90} />
+      <Hero />
     </Container>
   );
 };
