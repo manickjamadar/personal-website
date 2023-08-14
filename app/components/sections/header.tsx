f20e0e7c6f128463033579a9bbe212c0cf4f2987
Manick <manickware@gmail.com>
2023-08-14 12:57:59 +0530

    initial header component created

--- app/components/sections/header.tsx
+++ app/components/sections/header.tsx
@@ -1,11 +1,21 @@
 import LinkPath from "@/app/utils/linkPath";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between">
       <Image src={LinkPath.logo} width={50} height={50} alt="Main Logo" />
+      <div className="flex gap-8 items-center">
+        <nav className="flex gap-6 ">
+          <Link href={"/"}>Home</Link>
+          <Link href={"/"}>Skills</Link>
+          <Link href={"/"}>Work</Link>
+          <Link href={"/"}>Resume</Link>
+        </nav>
+        <button className="primary-btn">Book Meeting</button>
+      </div>
     </div>
   );
 };
