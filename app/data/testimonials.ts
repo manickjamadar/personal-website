be80c9f7611c37e16f096515dd7ecf3a4c0dd6a2
Manick <manickware@gmail.com>
2023-08-15 12:35:21 +0530

    initial testimonial data added

--- app/data/testimonials.ts
+++ app/data/testimonials.ts
@@ -5,3 +5,24 @@ export interface ITestimonial {
   profileUrl: string;
   quote: string;
 }
+
+const myTestimonials: ITestimonial[] = [
+  {
+    designation: "Founder & CEO at Nessvel",
+    imageUrl: "/assets/images/profile-1.png",
+    name: "Subhadip Maity",
+    profileUrl: "https://linkedin.com",
+    quote:
+      "Manick was a real pleasure to work with and we look forward to working with him again. He's definitely the kind of designer you can trust with a project from start to finish",
+  },
+  {
+    designation: "UI/UX designer at Promptyland",
+    imageUrl: "/assets/images/profile-2.png",
+    name: "Nasrin Khatun",
+    profileUrl: "https://linkedin.com",
+    quote:
+      "Manick was a real pleasure to work with and we look forward to working with him again. He's definitely the kind of designer you can trust with a project from start to finish",
+  },
+];
+
+export default myTestimonials;
