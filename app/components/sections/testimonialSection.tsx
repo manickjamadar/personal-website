7ea8b485a1e8749e8c053e0575a0f6b4e1729ad4
Manick <manickware@gmail.com>
2023-08-15 12:35:44 +0530

    testimonial carousel added in testiminoal section

--- app/components/sections/testimonialSection.tsx
+++ app/components/sections/testimonialSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SectionHeader from "../sectionHeader";
-import TestimonialCard from "../testimonialCard";
-import Spacer from "../spacer";
+import TestimonialCarousel from "../testimonialCarousel";
+import myTestimonials from "@/app/data/testimonials";
 
 const TestimonialSection = () => {
   return (
@@ -10,22 +10,7 @@ const TestimonialSection = () => {
         title="Testimonials"
         subtitle="People I've worked with said something about me"
       />
-      <TestimonialCard
-        testimonial={{
-          designation: "Founder & CEO at Nessvel",
-          imageUrl: "/assets/images/profile-1.png",
-          name: "Subhadip Maity",
-          profileUrl: "https://linkedin.com",
-          quote:
-            "Manick was a real pleasure to work with and we look forward to working with him again. He's definitely the kind of designer you can trust with a project from start to finish",
-        }}
-      />
-      <div className="flex gap-4">
-        <div className="w-3 h-3 rounded-full bg-primary-500"></div>
-        <div className="w-3 h-3 rounded-full bg-slate-500"></div>
-        <div className="w-3 h-3 rounded-full bg-slate-500"></div>
-        <div className="w-3 h-3 rounded-full bg-slate-500"></div>
-      </div>
+      <TestimonialCarousel testimonials={myTestimonials} />
     </div>
   );
 };
