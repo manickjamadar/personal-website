2b5bd7af4fd4477a44a231aa9bd595181e9a4d10
Manick <manickware@gmail.com>
2023-08-17 12:08:36 +0530

    nav link path added in link path

--- app/utils/linkPath.ts
+++ app/utils/linkPath.ts
@@ -1,4 +1,10 @@
 class LinkPath {
   static logo = "/assets/images/manick-logo.svg";
+  static home = "/";
+  static skills = "/#skills";
+  static projects = "/#projects";
+  static testimonials = "/#testimonials";
+  static faq = "/#faq";
+  static resume = "/assets/resume.pdf";
 }
 export default LinkPath;
