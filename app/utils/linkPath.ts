f4e433806d80c03b4b37f75a1a7d29754c352cbe
Manick <manickware@gmail.com>
2023-08-17 12:57:31 +0530

    social link urls added in linkpath

--- app/utils/linkPath.ts
+++ app/utils/linkPath.ts
@@ -6,5 +6,8 @@ class LinkPath {
   static testimonials = "/#testimonials";
   static faq = "/#faq";
   static resume = "/assets/resume.pdf";
+  static linkedIn = "https://linkedin.com/in/manickjamadar/";
+  static mail = "mailto:contact@manickjamadar.com";
+  static github = "https://github.com/manickjamadar";
 }
 export default LinkPath;
