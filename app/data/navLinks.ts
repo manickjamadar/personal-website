dfdad68c4d6a6da63f24e93e9d998689bad3c7b0
Manick <manickware@gmail.com>
2023-08-17 14:31:38 +0530

    removed resume from nav links

--- app/data/navLinks.ts
+++ app/data/navLinks.ts
@@ -2,14 +2,12 @@ import LinkPath from "../utils/linkPath";
 export interface INavLink {
   name: string;
   url: string;
-  target: "_self" | "_blank";
 }
 const navLinks: INavLink[] = [
-  { name: "Home", url: LinkPath.home, target: "_self" },
-  { name: "Skills", url: LinkPath.skills, target: "_self" },
-  { name: "Projects", url: LinkPath.projects, target: "_self" },
-  { name: "Testimonials", url: LinkPath.testimonials, target: "_self" },
-  { name: "FAQ", url: LinkPath.faq, target: "_self" },
-  { name: "Resume", url: LinkPath.resume, target: "_blank" },
+  { name: "Home", url: LinkPath.home },
+  { name: "Skills", url: LinkPath.skills },
+  { name: "Projects", url: LinkPath.projects },
+  { name: "Testimonials", url: LinkPath.testimonials },
+  { name: "FAQ", url: LinkPath.faq },
 ];
 export default navLinks;
