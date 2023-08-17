9a93b536406d7c6fa6831627c41b2bdf06a4e70a
Manick <manickware@gmail.com>
2023-08-17 13:55:53 +0530

    drawer component added in header

--- app/components/sections/header.tsx
+++ app/components/sections/header.tsx
@@ -1,25 +1,41 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Logo from "../logo";
 import navLinks from "@/app/data/navLinks";
 import { RiMenu4Fill } from "react-icons/ri";
+import { MdOutlineClose } from "react-icons/md";
+import Spacer from "../spacer";
+import SocialLinks from "../socialLinks";
+import Link from "next/link";
+import LinkPath from "@/app/utils/linkPath";
+import Drawer from "../drawer";
 const Header = () => {
+  const [drawerVisible, setDrawerVisible] = useState(false);
+  const openDrawer = () => setDrawerVisible(true);
+  const closeDrawer = () => setDrawerVisible(false);
   return (
-    <div className="flex items-center justify-between">
-      <Logo />
-      <div className="gap-8 items-center hidden min-[900px]:flex">
-        <nav className="flex gap-6 ">
-          {navLinks.map((link) => (
-            <a key={link.name} href={link.url} target={link.target}>
-              {link.name}
-            </a>
-          ))}
-        </nav>
-        <button className="primary-btn">Book Meeting</button>
+    <>
+      <div className="flex items-center justify-between">
+        <Logo />
+        <div className="gap-8 items-center hidden min-[900px]:flex">
+          <nav className="flex gap-6 ">
+            {navLinks.map((link) => (
+              <Link key={link.name} href={link.url} target={link.target}>
+                {link.name}
+              </Link>
+            ))}
+          </nav>
+          <button className="primary-btn">Book Meeting</button>
+        </div>
+        <div
+          className="text-white text-3xl min-[900px]:hidden cursor-pointer"
+          onClick={openDrawer}
+        >
+          <RiMenu4Fill />
+        </div>
       </div>
-      <div className="text-white text-3xl min-[900px]:hidden">
-        <RiMenu4Fill />
-      </div>
-    </div>
+      {drawerVisible && <Drawer links={navLinks} onClose={closeDrawer} />}
+    </>
   );
 };
 
