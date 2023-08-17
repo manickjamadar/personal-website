7b6d331fc726cd3e49c43b8b99f01f657e8475b1
Manick <manickware@gmail.com>
2023-08-17 12:20:56 +0530

    menu icon added in header

--- app/components/sections/header.tsx
+++ app/components/sections/header.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Logo from "../logo";
 import navLinks from "@/app/data/navLinks";
-
+import { RiMenu4Fill } from "react-icons/ri";
 const Header = () => {
   return (
     <div className="flex items-center justify-between">
       <Logo />
-      <div className="flex gap-8 items-center">
+      <div className="gap-8 items-center hidden min-[900px]:flex">
         <nav className="flex gap-6 ">
           {navLinks.map((link) => (
             <a key={link.name} href={link.url} target={link.target}>
@@ -15,6 +15,9 @@ const Header = () => {
           ))}
         </nav>
         <button className="primary-btn">Book Meeting</button>
+      </div>
+      <div className="text-white text-3xl min-[900px]:hidden">
+        <RiMenu4Fill />
       </div>
     </div>
   );
