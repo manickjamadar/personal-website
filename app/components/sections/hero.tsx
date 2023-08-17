0f6980cf98803dd14d4f798248cfcd0a9a42b575
Manick <manickware@gmail.com>
2023-08-17 19:19:30 +0530

    morphtexts added in hero component

--- app/components/sections/hero.tsx
+++ app/components/sections/hero.tsx
@@ -1,15 +1,16 @@
 import LinkPath from "@/app/utils/linkPath";
 import React from "react";
 import SocialLinks from "../socialLinks";
+import MorphTexts from "../MorphTexts";
 
 const Hero = () => {
   return (
     <div className="flex flex-col items-center gap-6 text-center">
       <p>Hello! 👋 My Name is</p>
       <h2 className="text-5xl font-bold">Manick Jamadar</h2>
-      <h1 className="font-bold text-6xl text-orange-gradient">
-        Full Stack Web Developer
-      </h1>
+      <MorphTexts
+        texts={["Full Stack Developer", "UI/UX Designer", "Problem Solver"]}
+      />
       <p>I build real apps for users to solve real problems</p>
       <div className="flex gap-4 flex-wrap justify-center">
         <button className="primary-btn">Book Meeting</button>
