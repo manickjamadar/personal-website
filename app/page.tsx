49b8bd62d5bce3a82a14c03aaea0d2a2d019a794
Manick <manickware@gmail.com>
2023-08-17 16:48:01 +0530

    detected scroll to change active link

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Container from "./components/Container";
 import Header from "./components/sections/header";
 import Hero from "./components/sections/hero";
@@ -11,12 +12,44 @@ import FAQSection from "./components/sections/faqSection";
 import ContactSection from "./components/sections/contactSection";
 
 const HomePage = () => {
+  const [activeLink, setActiveLink] = useState("/#");
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll("section"); // Assuming your sections have the <section> tag
+      const scrollPosition = window.scrollY;
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 100; // Adjust the offset if needed
+        const sectionBottom = sectionTop + section.offsetHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+          if (!section.getAttribute("id")) {
+            return;
+          }
+          const hash = `#${section.getAttribute("id")}`;
+          const actualHash = hash === "#home" ? "#" : hash;
+          window.history.replaceState(null, "", actualHash);
+          setActiveLink("/" + actualHash);
+        }
+      });
+    };
+
+    // Attach scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <Container>
-      <Header />
-      <Spacer vertical={90} />
-      <Hero />
-      <section id="specilization">
+      <Header activeLink={activeLink} />
+      <section id="home">
+        <Spacer vertical={90} />
+        <Hero />
+      </section>
+      <section>
         <Spacer vertical={90} />
         <SpecilizationSection />
       </section>
@@ -36,7 +69,7 @@ const HomePage = () => {
         <Spacer vertical={90} />
         <FAQSection />
       </section>
-      <section id="contact">
+      <section>
         <Spacer vertical={90} />
         <ContactSection />
       </section>
