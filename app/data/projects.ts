e5aa8a50f3ec217863b6707e18a9f1033ea3fac9
Manick <manickware@gmail.com>
2023-08-18 11:58:54 +0530

    intial real projects added

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,7 +1,12 @@
+import LinkPath from "../utils/linkPath";
 import { ISkill, Skill } from "./skills";
-
+export enum ProjectCategory {
+  mobileApp = "Mobile App",
+  webApp = "Web App",
+  design = "UI/UX",
+}
 export interface IProject {
-  category: "mobile app" | "web app" | "ui/ux";
+  category: ProjectCategory;
   title: string;
   description: string;
   imageUrl?: string;
@@ -12,30 +17,36 @@ export interface IProject {
 
 const myProjects: IProject[] = [
   {
-    category: "mobile app",
+    category: ProjectCategory.webApp,
     title: "Personal Website",
-    imageUrl: "/assets/images/project-1.png",
     description:
-      "A single-page application that helps filmmakers learn more about who their film might be suitable for",
-    skills: [Skill.firebase, Skill.html, Skill.nodeJs],
-    liveUrl: "https://localhost:3000",
-    sourceUrl: "https://github.com/manickjamadar",
+      "It's a digital mirror reflecting my devotion to software developement and design. Explore my journey, projects, and skills all in one sleek virtual space",
+    skills: [
+      Skill.typescript,
+      Skill.reactJs,
+      Skill.nextJs,
+      Skill.tailwind,
+      Skill.vercel,
+    ],
+    imageUrl: LinkPath.personalWebsiteImage,
+    liveUrl: LinkPath.personlWebsiteLive,
+    sourceUrl: LinkPath.personalWebsiteSource,
   },
   {
-    category: "web app",
-    title: "Performance Monitor ",
+    category: ProjectCategory.webApp,
+    title: "Prompty Land",
     description:
-      "A single-page application that helps filmmakers learn more about who their film might be suitable for",
-    skills: [Skill.firebase, Skill.html, Skill.nodeJs],
-    liveUrl: "https://localhost:3000",
-  },
-  {
-    category: "ui/ux",
-    title: "Rubikcon",
-    description:
-      "A single-page application that helps filmmakers learn more about who their film might be suitable for",
-    skills: [Skill.firebase, Skill.html, Skill.nodeJs],
-    sourceUrl: "https://github.com/manickjamadar",
+      "Promptyland is an AI Prompt marketplace for modern world to create, share and explore creative prompts",
+    skills: [
+      Skill.typescript,
+      Skill.reactJs,
+      Skill.nextJs,
+      Skill.tailwind,
+      Skill.mongodb,
+      Skill.vercel,
+    ],
+    imageUrl: LinkPath.promptylandImage,
+    liveUrl: LinkPath.promptylandLive,
   },
 ];
 export default myProjects;
