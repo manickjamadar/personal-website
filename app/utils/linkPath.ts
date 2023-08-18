eed2c3021ee568369a3953b1f2a0e33911a632e2
Manick <manickware@gmail.com>
2023-08-18 11:58:40 +0530

    projects links added

--- app/utils/linkPath.ts
+++ app/utils/linkPath.ts
@@ -10,5 +10,11 @@ class LinkPath {
   static linkedIn = "https://linkedin.com/in/manickjamadar/";
   static mail = "mailto:contact@manickjamadar.com";
   static github = "https://github.com/manickjamadar";
+  static personlWebsiteLive = "https://manickjamadar.com";
+  static personalWebsiteSource =
+    "https://github.com/manickjamadar/personal-website";
+  static personalWebsiteImage = "/assets/images/projects/personal-website.png";
+  static promptylandLive = "https://promptyland.com";
+  static promptylandImage = "/assets/images/projects/promptyland.png";
 }
 export default LinkPath;
