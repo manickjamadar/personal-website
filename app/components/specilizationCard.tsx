d182a2ea998c86ce8589c1f7b869110d7bf0b4c5
Manick <manickware@gmail.com>
2023-08-19 20:15:08 +0530

    redesigned specialization card

--- app/components/specilizationCard.tsx
+++ app/components/specilizationCard.tsx
@@ -6,10 +6,24 @@ interface Props {
 const SpecilizationCard: React.FC<Props> = ({ specilization }) => {
   const { description, icon, title } = specilization;
   return (
-    <div className="flex flex-col gap-4 max-w-xs items-start">
-      <div className="text-4xl text-primary">{icon}</div>
-      <h3 className="text-lg border-b-2 border-primary pb-2">{title}</h3>
-      <p className="text-sm text-gray-300">{description}</p>
+    <div
+      className="flex flex-col gap-4 max-w-xs items-start border border-slate-50 border-opacity-5 backdrop-filter backdrop-blur-sm rounded-md p-6"
+      style={{
+        background: specilization.color + "08",
+      }}
+    >
+      <div
+        className="text-xl p-3 bg-primary bg-opacity-10 text-primary border border-primary rounded-full"
+        style={{
+          background: specilization.color + "20",
+          borderColor: specilization.color,
+          color: specilization.color,
+        }}
+      >
+        {icon}
+      </div>
+      <h3 className="text-lg font-medium text-gray-200">{title}</h3>
+      <p className="text-sm text-gray-400">{description}</p>
     </div>
   );
 };
--- app/data/specilizations.tsx
+++ app/data/specilizations.tsx
@@ -1,28 +1,32 @@
-import { FaDatabase } from "react-icons/fa";
-import { MdDesktopWindows, MdDraw } from "react-icons/md";
+import { HiOutlinePaintBrush } from "react-icons/hi2";
+import { GoDatabase, GoDeviceDesktop } from "react-icons/go";
 export interface ISpecilization {
   title: string;
   description: string;
   icon: React.ReactNode;
+  color: string;
 }
 const mySpecilizations: ISpecilization[] = [
   {
     title: "Frontend Development",
     description:
       "A single-page application that helps filmmakers learn more about who their film might be suitable for.",
-    icon: <MdDesktopWindows />,
+    icon: <GoDeviceDesktop />,
+    color: "#bd3a71",
   },
   {
     title: "Backend Development",
     description:
       "A single-page application that helps filmmakers learn more about who their film might be suitable for.",
-    icon: <FaDatabase />,
+    icon: <GoDatabase />,
+    color: "#6655D0",
   },
   {
     title: "UI/UX Design",
     description:
       "A single-page application that helps filmmakers learn more about who their film might be suitable for.",
-    icon: <MdDraw />,
+    icon: <HiOutlinePaintBrush />,
+    color: "#0284C7",
   },
   //Upcoming
   //---------------------
