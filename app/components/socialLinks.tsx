e4714dc20706b724e6decf8e8a6cc41ea8e8c01f
Manick <manickware@gmail.com>
2023-08-21 13:13:41 +0530

    social media links hover effect added

--- app/components/socialLinks.tsx
+++ app/components/socialLinks.tsx
@@ -2,27 +2,24 @@ import React from "react";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import LinkPath from "../utils/linkPath";
-
+const links: { url: string; icon: React.ReactNode; name: string }[] = [
+  { url: LinkPath.linkedIn, icon: <FaLinkedinIn />, name: "LinkedIn" },
+  { url: LinkPath.mail, icon: <MdEmail />, name: "Mail" },
+  { url: LinkPath.github, icon: <FaGithub />, name: "Github" },
+];
 const SocialLinks = () => {
   return (
     <div className="flex gap-3">
-      <a
-        className="p-2 border rounded-full"
-        href={LinkPath.linkedIn}
-        target="_blank"
-      >
-        <FaLinkedinIn />
-      </a>
-      <a className="p-2 border rounded-full" href={LinkPath.mail}>
-        <MdEmail />
-      </a>
-      <a
-        className="p-2 border rounded-full"
-        href={LinkPath.github}
-        target="_blank"
-      >
-        <FaGithub />
-      </a>
+      {links.map(({ icon, name, url }) => (
+        <a
+          key={name}
+          className="p-2 text-slate-300 border border-slate-300 rounded-full hover:scale-105 hover:-translate-y-[2px] hover:border-white hover:text-white cursor-pointer transition-all duration-200 hover:shadow-[0_0px_5px_2px_rgba(255,255,255,0.2)]"
+          href={url}
+          target="_blank"
+        >
+          {icon}
+        </a>
+      ))}
     </div>
   );
 };
