414ec14be076fdea990448190c718b4609baa657
Manick <manickware@gmail.com>
2023-08-21 19:21:18 +0530

    added proper subtitle in project section

--- app/components/sections/projectSection.tsx
+++ app/components/sections/projectSection.tsx
@@ -6,14 +6,14 @@ import myProjects from "@/app/data/projects";
 
 const ProjectSection = () => {
   return (
-    <div className="flex flex-col gap-6 items-center">
+    <div className="flex flex-col gap-12 items-center">
       <SectionHeader
         title={
           <span>
-            My Creative <span className="text-orange-gradient">Projects</span>
+            Live <span className="text-orange-gradient">Projects</span>
           </span>
         }
-        subtitle="Check out my awesome creative projects"
+        subtitle="Solving real world problems by building stuff is my passion not my work"
       />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {myProjects.map((project) => (
