db95a75001d53113c8d900802289278180c6ad15
Manick <manickware@gmail.com>
2023-08-21 14:33:51 +0530

    added proper specilization subttile

--- app/components/sections/specilizationSection.tsx
+++ app/components/sections/specilizationSection.tsx
@@ -7,7 +7,7 @@ const SpecilizationSection = () => {
     <div className="flex gap-12 flex-col items-center">
       <SectionHeader
         title="Specilizations"
-        subtitle="I am specilized in various types of skills"
+        subtitle="I possess diverse expertise in various areas of app development"
       />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
         {mySpecilizations.map((specilization) => (
