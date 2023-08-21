330b2e9dafc50e184866224ac615f3e03eddd1d1
Manick <manickware@gmail.com>
2023-08-21 15:25:30 +0530

    specilization subttile rethinked

--- app/components/sections/specilizationSection.tsx
+++ app/components/sections/specilizationSection.tsx
@@ -7,7 +7,7 @@ const SpecilizationSection = () => {
     <div className="flex gap-12 flex-col items-center">
       <SectionHeader
         title="Specilizations"
-        subtitle="I possess diverse expertise in various areas of app development"
+        subtitle="I possess diverse expertise in various areas of software development"
       />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
         {mySpecilizations.map((specilization) => (
