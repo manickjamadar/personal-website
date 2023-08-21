8bc83ac421c16f5695878015c83886354a90b8ee
Manick <manickware@gmail.com>
2023-08-21 18:51:46 +0530

    titlte added semibold in section header

--- app/components/sectionHeader.tsx
+++ app/components/sectionHeader.tsx
@@ -7,7 +7,7 @@ const SectionHeader: React.FC<Props> = ({ title, subtitle }) => {
   return (
     <div className="flex flex-col gap-3 items-center text-center">
       <div>
-        <h2 className="text-2xl sm:text-3xl mb-2">{title}</h2>
+        <h2 className="text-2xl sm:text-3xl mb-2 font-semibold">{title}</h2>
         <div className="h-[3px] sm:h-[4px] bg-gradient-to-r from-transparent via-primary-500 to-transparent"></div>
       </div>
       <p className="text-slate-400 max-w-xs">{subtitle}</p>
