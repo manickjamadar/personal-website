96ff9d4a8f8d66160e0d8649bfdb7d5a6d02c5b2
Manick <manickware@gmail.com>
2023-08-21 11:31:56 +0530

    header made responsive

--- app/components/sections/header.tsx
+++ app/components/sections/header.tsx
@@ -19,9 +19,10 @@ const Header: React.FC<Props> = ({ activeLink = "/#", onLinkClick }) => {
   };
   return (
     <>
-      <div className="w-full mx-auto max-w-7xl px-8 sm:px-16 md:px-24 py-4 ">
+      <div className="w-full mx-auto max-w-7xl px-8 sm:px-16 md:px-24 py-3 md:py-4">
         <div className="flex items-center justify-between">
-          <Logo />
+          <Logo size={50} className="hidden sm:block" />
+          <Logo size={40} className="sm:hidden" />
           <div className="gap-8 items-center hidden min-[900px]:flex">
             <nav className="flex gap-6 ">
               {navLinks.map((link) => (
@@ -49,7 +50,7 @@ const Header: React.FC<Props> = ({ activeLink = "/#", onLinkClick }) => {
             <button className="primary-btn">Book Meeting</button>
           </div>
           <div
-            className="text-white text-3xl min-[900px]:hidden cursor-pointer"
+            className="text-white text-2xl sm:text-3xl min-[900px]:hidden cursor-pointer"
             onClick={openDrawer}
           >
             <RiMenu4Fill />
