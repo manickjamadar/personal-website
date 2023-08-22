4524ba9148e78746dcf635cdc820886d581796fc
Manick <manickware@gmail.com>
2023-08-22 10:05:49 +0530

    add responsive sizes in testimonial card

--- app/components/testimonialCard.tsx
+++ app/components/testimonialCard.tsx
@@ -10,24 +10,33 @@ interface Props {
 const TestimonialCard: React.FC<Props> = ({ testimonial }) => {
   const { designation, imageUrl, name, profileUrl, quote } = testimonial;
   return (
-    <div className="relative p-8 bg-primary bg-opacity-10 border border-gray-700  rounded-lg flex flex-col items-center max-w-xl backdrop-filter backdrop-blur">
+    <div className="relative glass p-8 rounded-lg flex flex-col items-center max-w-xl">
       <Image
         src={imageUrl}
         width={90}
         height={90}
         alt={`${name} Profile Picture`}
-        className="rounded-full object-cover border-4 border-primary-500 border-opacity-20 absolute -top-0 -translate-y-1/2"
+        className="hidden sm:block rounded-full object-cover border-4 border-gray-500 border-opacity-20 absolute -top-0 -translate-y-1/2"
+      />
+      <Image
+        src={imageUrl}
+        width={76}
+        height={76}
+        alt={`${name} Profile Picture`}
+        className="sm:hidden rounded-full object-cover border-4 border-gray-500 border-opacity-20 absolute -top-0 -translate-y-1/2"
       />
       <Spacer vertical={26} />
-      <div className="flex gap-3 items-center text-2xl">
+      <div className="flex gap-3 items-center text-xl sm:text-2xl">
         <h3>{name}</h3>
       </div>
       <Spacer vertical={6} />
-      <p className="text-base text-orange-gradient font-semibold">
+      <p className="text-base sm:text-lg text-orange-gradient font-bold">
         {designation}
       </p>
       <Spacer vertical={20} />
-      <p className="text-slate-300 text-center">&quot;{quote}&quot;</p>
+      <p className="text-sm sm:text-base text-slate-300 text-center">
+        &quot;{quote}&quot;
+      </p>
     </div>
   );
 };
