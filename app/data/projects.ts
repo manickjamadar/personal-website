1f24a75f74d668b178f647b04a2e9792ecb1f80f
Manick <manickware@gmail.com>
2023-08-30 13:20:00 +0530

    rearragned projects data

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -19,23 +19,6 @@ export interface IProject {
 const myProjects: IProject[] = [
   {
     category: ProjectCategory.webApp,
-    title: "Personal Website",
-    description:
-      "It's a digital mirror reflecting my devotion to software developement and design. Explore my journey, projects, and skills all in one sleek virtual space",
-    skills: [
-      Skill.typescript,
-      Skill.reactJs,
-      Skill.framerMotion,
-      Skill.nextJs,
-      Skill.tailwind,
-      Skill.vercel,
-    ],
-    imageUrl: LinkPath.personalWebsiteImage,
-    liveUrl: LinkPath.personlWebsiteLive,
-    sourceUrl: LinkPath.personalWebsiteSource,
-  },
-  {
-    category: ProjectCategory.webApp,
     title: "Prompty Land",
     description:
       "Promptyland is an AI Prompt marketplace for modern world to create, share and explore creative prompts",
@@ -50,6 +33,23 @@ const myProjects: IProject[] = [
     ],
     imageUrl: LinkPath.promptylandImage,
     liveUrl: LinkPath.promptylandLive,
+  },
+  {
+    category: ProjectCategory.webApp,
+    title: "Personal Website",
+    description:
+      "It's a digital mirror reflecting my devotion to software developement and design. Explore my journey, projects, and skills all in one sleek virtual space",
+    skills: [
+      Skill.typescript,
+      Skill.reactJs,
+      Skill.framerMotion,
+      Skill.nextJs,
+      Skill.tailwind,
+      Skill.vercel,
+    ],
+    imageUrl: LinkPath.personalWebsiteImage,
+    liveUrl: LinkPath.personlWebsiteLive,
+    sourceUrl: LinkPath.personalWebsiteSource,
   },
   {
     category: ProjectCategory.webApp,
